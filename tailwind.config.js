05a2c27559a93a09b22d14b1f48609718e63aab9
Andri Óskarsson <account+github@andri.dk>
2023-12-18 10:37:06 +0100

    live reloading works

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,13 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    // Requires that we have npm package installed, later.
-    //
-    // plugin(function({ addVariant }) {
-    //   addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &']),
-    //   addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &']),
-    //   addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &']),
-    //   addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &']),
-    //   addVariant('htmx-indicator', ['&.htmx-indicator',    '.htmx-indicator &']),
-    // })
+    plugin(function({ addVariant }) {
+      addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
+      addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &'])
+      addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
+      addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &'])
+      addVariant('htmx-indicator', ['&.htmx-indicator',    '.htmx-indicator &'])
+    })
   ],
 }
 
--- web/layouts/body_templ.go
+++ web/layouts/body_templ.go
@@ -27,7 +27,7 @@ func RootLayout() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var2 := `Test`
+		templ_7745c5c3_Var2 := `App 6`
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
